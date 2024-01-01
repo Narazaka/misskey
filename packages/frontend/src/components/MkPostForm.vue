526b2b91a1f1d541d50d533030baabe6a1626bd3
Narazaka <info@narazaka.net>
2024-01-01 22:06:22 +0900

    disable cwNotationRequired

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -688,7 +688,7 @@ function deleteDraft() {
 }
 
 async function post(ev?: MouseEvent) {
-	if (useCw.value && (cw.value == null || cw.value.trim() === '')) {
+	if (false && useCw.value && (cw.value == null || cw.value.trim() === '')) {
 		os.alert({
 			type: 'error',
 			text: i18n.ts.cwNotationRequired,
